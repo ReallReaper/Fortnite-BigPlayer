82ef56abbc56f9fbddad5b1f29470c05a0497874
Real Reaper <95001569+ReallReaper@users.noreply.github.com>
2022-11-09 11:49:35 -0300

    Add files via upload

---
+++ BigPlayer.cpp
@@ -0,0 +1,5 @@
+if (BigPlayerUwU)
+{
+    Writee<Vector3>(mesh + 0x158, Vector3(BigPlayersScale, BigPlayersScale, BigPlayersScale));
+    Writee<Vector3>(mesh + 0x158, Vector3(BigPlayersScale, BigPlayersScale, BigPlayersScale));
+}
